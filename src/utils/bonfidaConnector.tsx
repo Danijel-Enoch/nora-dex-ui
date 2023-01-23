bf8e95c39bb73cdad140581e419f82be02c7054b
Danijel-Enoch <enochdanijel@gmail.com>
2023-01-23 20:06:49 +0100

    UPDATE

--- src/utils/bonfidaConnector.tsx
+++ src/utils/bonfidaConnector.tsx
@@ -1,26 +1,26 @@
 import { BonfidaTrade } from './types';
 
 export default class BonfidaApi {
-  static URL: string = 'https://dry-ravine-67635.herokuapp.com';
+	static URL: string = 'https://dropcopy.io/analytics/';
 
-  static async get(path: string) {
-    try {
-      const response = await fetch(this.URL + path);
-      if (response.ok) {
-        const responseJson = await response.json();
-        return responseJson.success ? responseJson.data : null;
-      }
-    } catch (err) {
-      console.log(`Error fetching from Bonfida API ${path}: ${err}`);
-    }
-    return null;
-  }
+	static async get(path: string) {
+		try {
+			const response = await fetch(this.URL + path);
+			if (response.ok) {
+				const responseJson = await response.json();
+				return responseJson.success ? responseJson.data : null;
+			}
+		} catch (err) {
+			console.log(`Error fetching from Bonfida API ${path}: ${err}`);
+		}
+		return null;
+	}
 
-  static async getRecentTrades(
-    marketAddress: string,
-  ): Promise<BonfidaTrade[] | null> {
-    return BonfidaApi.get(`trades/address/${marketAddress}`);
-  }
+	static async getRecentTrades(
+		marketAddress: string,
+	): Promise<BonfidaTrade[] | null> {
+		return BonfidaApi.get(`trades/address/${marketAddress}`);
+	}
 }
 
-export const BONFIDA_DATA_FEED = 'https://dry-ravine-67635.herokuapp.com/tv';
+export const BONFIDA_DATA_FEED = 'https://dropcopy.io/analytics/tv';
