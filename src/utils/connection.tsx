72c9cb133b341a366b4f0b75a7d6574c799189f2
Danijel-Enoch <enochdanijel@gmail.com>
2023-01-23 20:22:27 +0100

    added market config

--- src/utils/connection.tsx
+++ src/utils/connection.tsx
@@ -6,178 +6,188 @@ import tuple from 'immutable-tuple';
 import { ConnectionContextValues, EndpointInfo } from './types';
 
 export const ENDPOINTS: EndpointInfo[] = [
-  {
-    name: 'mainnet-beta',
-    endpoint:
-      process.env.REACT_APP_SOLANA_RPC_ENDPOINT ||
-      'https://solana-mainnet.g.alchemy.com/v2/r3-VIvtGqsTO1l2Oh_0PFrmAkHit_bFO',
-    custom: false,
-  },
-  { name: 'localnet', endpoint: 'http://127.0.0.1:8899', custom: false },
+	{
+		name: 'mainnet-beta',
+		endpoint:
+			'https://nd-842-336-972.p2pify.com/e69d7edc17bbeceaac7986913bdc2a98' ||
+			'https://solana-mainnet.g.alchemy.com/v2/r3-VIvtGqsTO1l2Oh_0PFrmAkHit_bFO',
+		custom: false,
+	},
+	{ name: 'localnet', endpoint: 'http://127.0.0.1:8899', custom: false },
 ];
 
 const accountListenerCount = new Map();
 
 const ConnectionContext: React.Context<null | ConnectionContextValues> =
-  React.createContext<null | ConnectionContextValues>(null);
+	React.createContext<null | ConnectionContextValues>(null);
 
 export function ConnectionProvider({ children }) {
-  const [endpoint, setEndpoint] = useLocalStorageState<string>(
-    'connectionEndpts',
-    ENDPOINTS[0].endpoint,
-  );
-  const [customEndpoints, setCustomEndpoints] = useLocalStorageState<
-    EndpointInfo[]
-  >('customConnectionEndpoints', []);
-  const availableEndpoints = ENDPOINTS.concat(customEndpoints);
+	const [endpoint, setEndpoint] = useLocalStorageState<string>(
+		'connectionEndpts',
+		ENDPOINTS[0].endpoint,
+	);
+	const [customEndpoints, setCustomEndpoints] = useLocalStorageState<
+		EndpointInfo[]
+	>('customConnectionEndpoints', []);
+	const availableEndpoints = ENDPOINTS.concat(customEndpoints);
 
-  const connection = useMemo(
-    () => new Connection(endpoint, 'recent'),
-    [endpoint],
-  );
-  const sendConnection = useMemo(
-    () => new Connection(endpoint, 'recent'),
-    [endpoint],
-  );
+	const connection = useMemo(
+		() => new Connection(endpoint, 'recent'),
+		[endpoint],
+	);
+	const sendConnection = useMemo(
+		() => new Connection(endpoint, 'recent'),
+		[endpoint],
+	);
 
-  // The websocket library solana/web3.js uses closes its websocket connection when the subscription list
-  // is empty after opening its first time, preventing subsequent subscriptions from receiving responses.
-  // This is a hack to prevent the list from every getting empty
-  useEffect(() => {
-    const id = connection.onAccountChange(new Account().publicKey, () => {});
-    return () => {
-      connection.removeAccountChangeListener(id);
-    };
-  }, [connection]);
+	// The websocket library solana/web3.js uses closes its websocket connection when the subscription list
+	// is empty after opening its first time, preventing subsequent subscriptions from receiving responses.
+	// This is a hack to prevent the list from every getting empty
+	useEffect(() => {
+		const id = connection.onAccountChange(
+			new Account().publicKey,
+			() => {},
+		);
+		return () => {
+			connection.removeAccountChangeListener(id);
+		};
+	}, [connection]);
 
-  useEffect(() => {
-    const id = connection.onSlotChange(() => null);
-    return () => {
-      connection.removeSlotChangeListener(id);
-    };
-  }, [connection]);
+	useEffect(() => {
+		const id = connection.onSlotChange(() => null);
+		return () => {
+			connection.removeSlotChangeListener(id);
+		};
+	}, [connection]);
 
-  useEffect(() => {
-    const id = sendConnection.onAccountChange(
-      new Account().publicKey,
-      () => {},
-    );
-    return () => {
-      sendConnection.removeAccountChangeListener(id);
-    };
-  }, [sendConnection]);
+	useEffect(() => {
+		const id = sendConnection.onAccountChange(
+			new Account().publicKey,
+			() => {},
+		);
+		return () => {
+			sendConnection.removeAccountChangeListener(id);
+		};
+	}, [sendConnection]);
 
-  useEffect(() => {
-    const id = sendConnection.onSlotChange(() => null);
-    return () => {
-      sendConnection.removeSlotChangeListener(id);
-    };
-  }, [sendConnection]);
+	useEffect(() => {
+		const id = sendConnection.onSlotChange(() => null);
+		return () => {
+			sendConnection.removeSlotChangeListener(id);
+		};
+	}, [sendConnection]);
 
-  return (
-    <ConnectionContext.Provider
-      value={{
-        endpoint,
-        setEndpoint,
-        connection,
-        sendConnection,
-        availableEndpoints,
-        setCustomEndpoints,
-      }}
-    >
-      {children}
-    </ConnectionContext.Provider>
-  );
+	return (
+		<ConnectionContext.Provider
+			value={{
+				endpoint,
+				setEndpoint,
+				connection,
+				sendConnection,
+				availableEndpoints,
+				setCustomEndpoints,
+			}}
+		>
+			{children}
+		</ConnectionContext.Provider>
+	);
 }
 
 export function useConnection() {
-  const context = useContext(ConnectionContext);
-  if (!context) {
-    throw new Error('Missing connection context');
-  }
-  return context.connection;
+	const context = useContext(ConnectionContext);
+	if (!context) {
+		throw new Error('Missing connection context');
+	}
+	return context.connection;
 }
 
 export function useSendConnection() {
-  const context = useContext(ConnectionContext);
-  if (!context) {
-    throw new Error('Missing connection context');
-  }
-  return context.sendConnection;
+	const context = useContext(ConnectionContext);
+	if (!context) {
+		throw new Error('Missing connection context');
+	}
+	return context.sendConnection;
 }
 
 export function useConnectionConfig() {
-  const context = useContext(ConnectionContext);
-  if (!context) {
-    throw new Error('Missing connection context');
-  }
-  return {
-    endpoint: context.endpoint,
-    endpointInfo: context.availableEndpoints.find(
-      (info) => info.endpoint === context.endpoint,
-    ),
-    setEndpoint: context.setEndpoint,
-    availableEndpoints: context.availableEndpoints,
-    setCustomEndpoints: context.setCustomEndpoints,
-  };
+	const context = useContext(ConnectionContext);
+	if (!context) {
+		throw new Error('Missing connection context');
+	}
+	return {
+		endpoint: context.endpoint,
+		endpointInfo: context.availableEndpoints.find(
+			(info) => info.endpoint === context.endpoint,
+		),
+		setEndpoint: context.setEndpoint,
+		availableEndpoints: context.availableEndpoints,
+		setCustomEndpoints: context.setCustomEndpoints,
+	};
 }
 
 export function useAccountInfo(
-  publicKey: PublicKey | undefined | null,
+	publicKey: PublicKey | undefined | null,
 ): [AccountInfo<Buffer> | null | undefined, boolean] {
-  const connection = useConnection();
-  const cacheKey = tuple(connection, publicKey?.toBase58());
-  const [accountInfo, loaded] = useAsyncData<AccountInfo<Buffer> | null>(
-    async () => (publicKey ? connection.getAccountInfo(publicKey) : null),
-    cacheKey,
-    { refreshInterval: 60_000 },
-  );
-  useEffect(() => {
-    if (!publicKey) {
-      return;
-    }
-    if (accountListenerCount.has(cacheKey)) {
-      let currentItem = accountListenerCount.get(cacheKey);
-      ++currentItem.count;
-    } else {
-      let previousInfo: AccountInfo<Buffer> | null = null;
-      const subscriptionId = connection.onAccountChange(publicKey, (info) => {
-        if (
-          !previousInfo ||
-          !previousInfo.data.equals(info.data) ||
-          previousInfo.lamports !== info.lamports
-        ) {
-          previousInfo = info;
-          setCache(cacheKey, info);
-        }
-      });
-      accountListenerCount.set(cacheKey, { count: 1, subscriptionId });
-    }
-    return () => {
-      let currentItem = accountListenerCount.get(cacheKey);
-      let nextCount = currentItem.count - 1;
-      if (nextCount <= 0) {
-        connection.removeAccountChangeListener(currentItem.subscriptionId);
-        accountListenerCount.delete(cacheKey);
-      } else {
-        --currentItem.count;
-      }
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cacheKey]);
-  const previousInfoRef = useRef<AccountInfo<Buffer> | null | undefined>(null);
-  if (
-    !accountInfo ||
-    !previousInfoRef.current ||
-    !previousInfoRef.current.data.equals(accountInfo.data) ||
-    previousInfoRef.current.lamports !== accountInfo.lamports
-  ) {
-    previousInfoRef.current = accountInfo;
-  }
-  return [previousInfoRef.current, loaded];
+	const connection = useConnection();
+	const cacheKey = tuple(connection, publicKey?.toBase58());
+	const [accountInfo, loaded] = useAsyncData<AccountInfo<Buffer> | null>(
+		async () => (publicKey ? connection.getAccountInfo(publicKey) : null),
+		cacheKey,
+		{ refreshInterval: 60_000 },
+	);
+	useEffect(() => {
+		if (!publicKey) {
+			return;
+		}
+		if (accountListenerCount.has(cacheKey)) {
+			let currentItem = accountListenerCount.get(cacheKey);
+			++currentItem.count;
+		} else {
+			let previousInfo: AccountInfo<Buffer> | null = null;
+			const subscriptionId = connection.onAccountChange(
+				publicKey,
+				(info) => {
+					if (
+						!previousInfo ||
+						!previousInfo.data.equals(info.data) ||
+						previousInfo.lamports !== info.lamports
+					) {
+						previousInfo = info;
+						setCache(cacheKey, info);
+					}
+				},
+			);
+			accountListenerCount.set(cacheKey, { count: 1, subscriptionId });
+		}
+		return () => {
+			let currentItem = accountListenerCount.get(cacheKey);
+			let nextCount = currentItem.count - 1;
+			if (nextCount <= 0) {
+				connection.removeAccountChangeListener(
+					currentItem.subscriptionId,
+				);
+				accountListenerCount.delete(cacheKey);
+			} else {
+				--currentItem.count;
+			}
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [cacheKey]);
+	const previousInfoRef = useRef<AccountInfo<Buffer> | null | undefined>(
+		null,
+	);
+	if (
+		!accountInfo ||
+		!previousInfoRef.current ||
+		!previousInfoRef.current.data.equals(accountInfo.data) ||
+		previousInfoRef.current.lamports !== accountInfo.lamports
+	) {
+		previousInfoRef.current = accountInfo;
+	}
+	return [previousInfoRef.current, loaded];
 }
 
 export function useAccountData(publicKey) {
-  const [accountInfo] = useAccountInfo(publicKey);
-  return accountInfo && accountInfo.data;
+	const [accountInfo] = useAccountInfo(publicKey);
+	return accountInfo && accountInfo.data;
 }
---
+++ src/utils/const/markets.config.ts
@@ -0,0 +1,58 @@
+import { PublicKey } from '@solana/web3.js';
+
+export const _MARKETS = [
+  {
+    name: 'SOL/USDT',
+    deprecated: false,
+    address: new PublicKey('2AdaV97p6SfkuMQJdu8DHhBhmJe7oWdvbm52MJfYQmfA'),
+    programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  },
+  {
+    name: 'SOL/USDC',
+    deprecated: false,
+    address: new PublicKey('8BnEgHoWFysVcuFFX7QztDmzuH8r5ZFvyP3sYwn1XTh6'),
+    programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  },
+  {
+    name: 'RAY/USDC',
+    deprecated: false,
+    address: new PublicKey('DZjbn4XC8qoHKikZqzmhemykVzmossoayV9ffbsUqxVj'),
+    programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  },
+  {
+    name: 'RAY/USDT',
+    deprecated: false,
+    address: new PublicKey('GpHbiJJ9VHiuHVXeoet121Utrbm1CSNNzYrBKB8Xz2oz'),
+    programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  },
+  {
+    name: 'MNGO/USDC',
+    deprecated: false,
+    address: new PublicKey('3NnxQvDcZXputNMxaxsGvqiKpqgPfSYXpNigZNFcknmD'),
+    programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  },
+  {
+    name: 'ETH/USDT',
+    deprecated: false,
+    address: new PublicKey('BbJgE7HZMaDp5NTYvRh5jZSkQPVDTU8ubPFtpogUkEj4'),
+    programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  },
+  {
+    name: 'BONK/USDT',
+    deprecated: false,
+    address: new PublicKey('8PhnCfgqpgFM7ZJvttGdBVMXHuU4Q23ACxCvWkbs1M71'),
+    programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  }
+  // ,{
+  //   name: 'BTC/USDT',
+  //   deprecated: false,
+  //   address: new PublicKey('C1EuT9VokAKLiW7i2ASnZUvxDoKuKkCpDDeNxAptuNe4'),
+  //   programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  // },
+  // {
+  //   name: 'BTC/USDC',
+  //   deprecated: false,
+  //   address: new PublicKey('A8YFbxQYFVqKZaoYJLLUVcQiWP7G2MeEgW5wsAQgMvFw'),
+  //   programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  // }
+];
