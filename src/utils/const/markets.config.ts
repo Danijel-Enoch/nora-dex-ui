e1a115a97d97c32775672b4c991bd065b4799889
drsaint1 <100613167+drsaint1@users.noreply.github.com>
2023-02-01 19:34:22 +0100

    first commit

--- src/utils/const/markets.config.ts
+++ src/utils/const/markets.config.ts
@@ -32,11 +32,30 @@ export const _MARKETS = [
     programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
   },
   {
+    name: 'SDOGE/USDC',
+    deprecated: false,
+    address: new PublicKey('Gmjq4394AB5eVtL4ELTraDVgXAn62Mk4r58H3xW5aTAe'),
+    programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  },
+  {
+    name: 'ARB/USDC',
+    deprecated: false,
+    address: new PublicKey('B5xCudgPvVetTL3M4ExtPuAFTnCr2hdQ3cte9LhGAuBee'),
+    programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  },
+  {
+    name: 'ORCA/USDC',
+    deprecated: false,
+    address: new PublicKey('BEhRuJZiKwTdVTsGYjbHRh9RmGbKBtT6xo7yPqxLiSSY'),
+    programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
+  },
+  {
     name: 'ETH/USDT',
     deprecated: false,
     address: new PublicKey('BbJgE7HZMaDp5NTYvRh5jZSkQPVDTU8ubPFtpogUkEj4'),
     programId: new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'),
   },
+  
   {
     name: 'BONK/USDC',
     deprecated: false,
